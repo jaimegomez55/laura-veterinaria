1bb6f356d006c1dfb360a8529e60495ec056b37e
jaimegomez55 <jgdealvare@gmail.com>
2025-12-15 13:38:45 +0100

    tb

--- src/pages/home/components/Services.tsx
+++ src/pages/home/components/Services.tsx
@@ -79,7 +79,7 @@ export default function Services() {
             Un acompañamiento nutricional pensado para tu perro
           </h2>
           <div className="w-20 h-1 bg-[#f2bac9] mx-auto mb-6"></div>
-          <p className="text-xl text-gray-600 max-w-3xl mx-auto">
+         <p className="text-xl text-gray-600 max-w-3xl mx-auto">
             Cada perro es único, y su alimentación también debería serlo. Aquí encontrarás laa metologia con la que te acompaño paso a paso para mejorar su bienestar a través de una nutrición adaptada a su caso concreto.,
         </div>
 
